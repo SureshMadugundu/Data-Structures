019307f6b21c16f1c63f9a7ba840cfe01c156489
Suresh Madugundu <madugundusuresh15@gmail.com>
2025-08-26 12:57:11 +0530

    modified SLL insertion at K node

--- LinkedList/SLL_Insertion.c
+++ LinkedList/SLL_Insertion.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 struct node
 {
     int data;
     struct node * next;
 };
-
 void printLL(struct node * head)
 {
     struct node * temp = head;
@@ -17,62 +15,73 @@ void printLL(struct node * head)
     }
     printf("NULL\n");
 }
-
-struct node * insertAtBegining(struct node * head, int value)
+struct node * insHead(struct node * head, int value)
 {
-    printf("Inserting a node at the begining of SLL\n");
     struct node * newnode = (struct node *)malloc(sizeof(struct node));
     newnode -> data = value;
     newnode -> next = head;
     return newnode;
 }
-struct node * insertAtEnding(struct node * head, int value)
+struct node * insTail(struct node * head, int value)
 {
-    printf("Inserting a node at the end of SLL\n");
     struct node * temp = head;
-    while(temp -> next != NULL)
-    {
-        temp = temp -> next;
-    }
     struct node * newnode = (struct node *)malloc(sizeof(struct node));
-    temp -> next = newnode;
     newnode -> data = value;
     newnode -> next = NULL;
-
-    return head;
-}
-struct node * insertAtPosition(struct node * head, int position, int value)
-{
-    struct node * temp = head;
-    int count = 1;
-    if(position == 1)
-    {
-        struct node * newnode = (struct node *)malloc(sizeof(struct node));
-        newnode -> data = value;
-        newnode -> next = head;
-        return newnode;
-    }
     while(temp -> next != NULL)
     {
-        count ++;
-        struct node * newnode = (struct node *)malloc(sizeof(struct node));
-        if(count == position - 1)
-        {
-            newnode -> data = value;
-            newnode -> next = temp -> next;
-            temp -> next = newnode;
-            break;
-        }
         temp = temp -> next;
     }
+    temp -> next = newnode;
+    return head;
+}
+struct node *insKNode(struct node *head, int k, int value) {
+    // Insert at head (position 1)
+    if (k == 1) {
+        struct node *newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = value;
+        newnode->next = head;
+        return newnode;
+    }
+
+    struct node *temp = head;
+    int count = 1;
+
+    // Traverse to (k-1)th node
+    while (temp != NULL && count < k - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = value;
+
+    if (temp == NULL) {
+        // If k is greater than length → insert at end
+        struct node *last = head;
+        if (last == NULL) {
+            // empty list case
+            return newnode;
+        }
+        while (last->next != NULL) {
+            last = last->next;
+        }
+        last->next = newnode;
+        newnode->next = NULL;
+    } else {
+        // Normal insertion at position k
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+
     return head;
 }
 int main()
 {
-    struct node * head = NULL, *temp = NULL, * newnode;
     int nodes;
     scanf("%d",&nodes);
 
+    struct node * head = NULL, *temp = NULL, *newnode = NULL;
     for(int i = 0 ; i < nodes ; i++)
     {
         newnode = (struct node *)malloc(sizeof(struct node));
@@ -81,7 +90,7 @@ int main()
 
         if(head == NULL)
         {
-            head = temp = newnode;
+            temp = head = newnode;
         }
         else
         {
@@ -89,8 +98,7 @@ int main()
             temp = newnode;
         }
     }
-    printLL(head);
-    struct node * pointer = insertAtPosition(head,5,100);
-    printLL(pointer);
+    temp = insKNode(head,5,13);
+    printLL(temp);
     return 0;
 }
